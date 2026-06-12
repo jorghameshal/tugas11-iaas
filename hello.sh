570ac1b1140af0f0c338340fa7f52961ac69842f
jorghameshal <rjorghameshal@gmail.com>
2026-06-12 13:21:37 +0700

    Create hello.sh

---
+++ hello.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "======================================="
+echo "        TUGAS IAAS AZURE VM"
+echo "======================================="
+echo "Kelompok:"
+echo "1. Jorgha Meshal Ramadhan (103012300113)"
+echo "2. Mochammad Rizqy Arafat (103012300192)"
+echo "3. Ahmad Fawzan Makkarumpa (103012300387)"
+echo "4. Dewanda Wijayarajasa Kila (103012300130)"
+echo "======================================="
+echo "Tanggal dan Waktu Server:"
+date
+echo "======================================="
+echo "Hostname Server:"
+hostname
+echo "======================================="
+echo "Script Bash Berhasil Dijalankan!"
